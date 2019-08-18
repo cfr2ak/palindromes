64f2055ac1a53b1f13da73322d0babdf5c7928e0
yoon gun <cfr2ak@gmail.com>
2019-08-18 15:33:45 +0900

    :tada: First commit

---
+++ palindromes.test.js
@@ -0,0 +1,7 @@
+const palindromes = require('./palindromes');
+
+describe('palindromes()', () => {
+    it('correctly identifies on-word palindromes', () => {
+        expect(palindromes('madam')).toEqual(['madam']);
+    });
+});
