f5e093bad58a710a82e7d5c0fab3cf12359555a2
yoon gun <cfr2ak@gmail.com>
2019-08-19 20:25:54 +0900

    :wrench: Initial Prettier setup

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -3,5 +3,6 @@ const palindromes = require('../palindromes');
 describe('palindromes()', () => {
     it('correctly identifies on-word palindromes', () => {
         expect(palindromes('madam')).toEqual(['madam']);
+        expect(palindromes('racecar')).toEqual(['racecar']);
     });
 });
