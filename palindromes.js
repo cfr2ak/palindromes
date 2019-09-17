ebc7a59223288467dce284e3847ace7d4c440b35
Yoon <gun.yoon@awl.co.jp>
2019-09-17 17:46:45 +0900

    :bulb:

--- palindromes.js
+++ palindromes.js
@@ -1,3 +1,10 @@
-module.exports = (str) => {
-    return [str];
+module.exports = str => {
+  const noPunctuationString = str.replace(/[ !,]/g, '').toLowerCase();
+  const splitString = noPunctuationString.split('');
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join('');
+  if (noPunctuationString === joinArray) {
+    return [noPunctuationString];
+  }
+  return [];
 };
--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -1,8 +1,26 @@
 const palindromes = require('../palindromes');
 
 describe('palindromes()', () => {
-    it('correctly identifies on-word palindromes', () => {
-        expect(palindromes('madam')).toEqual(['madam']);
-        expect(palindromes('racecar')).toEqual(['racecar']);
-    });
+  it('correctly identifies on-word palindromes', () => {
+    expect(palindromes('madam')).toEqual(['madam']);
+    expect(palindromes('racecar')).toEqual(['racecar']);
+  });
+
+  it('returns an empty array when given no palindromes', () => {
+    expect(palindromes('tic tac toe')).toEqual([]);
+  });
+
+  it('ignores casing', () => {
+    expect(palindromes('WoW')).toEqual(['wow']);
+  });
+
+  it('ignores punctuation', () => {
+    expect(palindromes('yo, banana boy!')).toEqual(['yobananaboy']);
+  });
+
+  it('detects multi-word palindormes', () => {
+    expect(palindromes('A man, a plan, a canal, Panama')).toEqual([
+      'amanaplanacanalpanama',
+    ]);
+  });
 });
